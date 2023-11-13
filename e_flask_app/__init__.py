6d80d178be47f03a4eafde8382d917259f656893
Justin Triplett <justin.triplett@gmail.com>
2023-11-13 16:13:21 -0500

    add ProxyFix

--- e_flask_app/__init__.py
+++ e_flask_app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from config import Config
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
     from . import views
 
